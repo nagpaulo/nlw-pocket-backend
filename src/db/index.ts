55f899ac5dabc019d5e456464d62d621ab008b47
Paulo Roberto Mesquita <pauloroberto@ajtec.eu>
2024-09-12 13:19:25 -0300

    first commit

---
+++ src/db/index.ts
@@ -0,0 +1,7 @@
+import postgres from "postgres";
+import * as schema from "./schema";
+import { env } from "../env";
+import { drizzle } from "drizzle-orm/postgres-js";
+
+export const client = postgres(env.DATABASE_URL);
+export const db = drizzle(client, { schema, logger: true });
